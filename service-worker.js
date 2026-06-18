509e9b86380be8c70b80db5bc8589b1371f91c5c
kdse1227 <kdse1227@gmail.com>
2026-06-18 19:33:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -21,20 +21,18 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
